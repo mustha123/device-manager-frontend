0a8196574a2402ca99af04d0b6aaf17baa4d656b
mustha123 <musthafaafraz2003@gmail.com>
2026-02-03 17:06:20 +0530

    api fix

--- src/api.js
+++ src/api.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL ||
-"https://device-management-backend-zbbp.onrender.com"
+"https://device-management-backend-zbbp.onrender.com",
+
+ headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
